885e7631a07349f1e3f833c21d3d3c2c6561e25f
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-01-11 18:18:44 +0500

    Implemented logic for the calculator
    in this commit
    - Copied the files calculate.js and operate.js into a logic/ directory.
    - Imported the files in calculator.js.
    - Implemented the event handlers to use the math logic from calculate.js and operate.js.

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { total: 0, next: null, operation: null };
+  }
+
+  buttonClicked = (event) => {
+    const btnName = event.target.innerText;
+    const answer = calculate(this.state, btnName);
+    this.setState(answer);
   }
 
   render() {
+    const { total, operation, next } = this.state;
     return (
       <div className="calculator">
         <div className="calculator__wrap">
           <div className="calculator__display">
-            0
+            {total}
+            {operation}
+            {next}
           </div>
           <div className="calculatorUI">
-            <button type="button" value="clear">AC</button>
-            <button type="button" value="pos-neg">+/-</button>
-            <button type="button" value="%">%</button>
-            <button type="button" value="/" className="btn--opaque">÷</button>
-            <button type="button" value="7">7</button>
-            <button type="button" value="8">8</button>
-            <button type="button" value="9">9</button>
-            <button type="button" value="*" className="btn--opaque">*</button>
-            <button type="button" value="4">4</button>
-            <button type="button" value="5">5</button>
-            <button type="button" value="6">6</button>
-            <button type="button" value="-" className="btn--opaque">-</button>
-            <button type="button" value="1">1</button>
-            <button type="button" value="2">2</button>
-            <button type="button" value="3">3</button>
-            <button type="button" value="+" className="btn--opaque">+</button>
-            <button type="button" value="0" className="btn--large">0</button>
-            <button type="button" value=".">.</button>
-            <button type="button" value="=" className="btn--opaque">=</button>
+            <button type="button" onClick={this.buttonClicked} value="clear">AC</button>
+            <button type="button" onClick={this.buttonClicked} value="pos-neg">+/-</button>
+            <button type="button" onClick={this.buttonClicked} value="%">%</button>
+            <button type="button" onClick={this.buttonClicked} value="/" className="btn--opaque">÷</button>
+            <button type="button" onClick={this.buttonClicked} value="7">7</button>
+            <button type="button" onClick={this.buttonClicked} value="8">8</button>
+            <button type="button" onClick={this.buttonClicked} value="9">9</button>
+            <button type="button" onClick={this.buttonClicked} value="x" className="btn--opaque">x</button>
+            <button type="button" onClick={this.buttonClicked} value="4">4</button>
+            <button type="button" onClick={this.buttonClicked} value="5">5</button>
+            <button type="button" onClick={this.buttonClicked} value="6">6</button>
+            <button type="button" onClick={this.buttonClicked} value="-" className="btn--opaque">-</button>
+            <button type="button" onClick={this.buttonClicked} value="1">1</button>
+            <button type="button" onClick={this.buttonClicked} value="2">2</button>
+            <button type="button" onClick={this.buttonClicked} value="3">3</button>
+            <button type="button" onClick={this.buttonClicked} value="+" className="btn--opaque">+</button>
+            <button type="button" onClick={this.buttonClicked} value="0" className="btn--large">0</button>
+            <button type="button" onClick={this.buttonClicked} value=".">.</button>
+            <button type="button" onClick={this.buttonClicked} value="=" className="btn--opaque">=</button>
           </div>
         </div>
       </div>
