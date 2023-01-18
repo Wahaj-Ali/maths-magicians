0f8781d76ec966ada1a7045adb25d76ac2b463c6
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-01-18 20:04:14 +0500

    removed linter errors

--- src/components.test/Home.test.js
+++ src/components.test/Home.test.js
@@ -3,14 +3,13 @@ import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Home from '../components/Home';
 
-
 describe('Check if it', () => {
-    it('renders Home component', () => {
-      render(<Home />);
-    });
+  it('renders Home component', () => {
+    render(<Home />);
   });
+});
 
-  it('renders correctly when there are no items', () => {
-    const tree = renderer.create(<Home />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+it('renders correctly when there are no items', () => {
+  const tree = renderer.create(<Home />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- src/components.test/Quote.test.js
+++ src/components.test/Quote.test.js
@@ -3,14 +3,13 @@ import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Quote from '../components/Quote';
 
-
 describe('Check if it', () => {
-    it('renders Quote component', () => {
-      render(<Quote />);
-    });
+  it('renders Quote component', () => {
+    render(<Quote />);
   });
+});
 
-  it('renders correctly when there are no items', () => {
-    const tree = renderer.create(<Quote />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+it('renders correctly when there are no items', () => {
+  const tree = renderer.create(<Quote />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
