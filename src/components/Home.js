5df1fc6bacbb48091ea25fad4b643ea2f60799b0
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-01 22:20:49 +0500

    Add style to make a responsive design for the website.
    In this commit
    - Edit style for the components of the app to make a responsive design.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => (
-  <div>
+  <>
     <div className="homePage">
       <h2 className="homeHeading">Welcome to our page</h2>
       <p className="homeParagraph">
-      Mathematics is the science of structure, order, and relation that has evolved from elemental practices of counting, measuring, and describing the shapes of objects. It deals with logical reasoning and quantitative calculation, and its development has involved an increasing degree of idealization and abstraction of its subject matter.
+        Mathematics is the science of structure, order, and relation
+        that has evolved from elemental practices of counting, measuring,
+        and describing the shapes of objects. It deals with logical reasoning
+        and quantitative calculation, and its development has involved an increasing
+        degree of idealization and abstraction of its subject matter.
       </p>
       <p className="homeParagraph">
-      Since the 17th century, mathematics has been an indispensable adjunct to the physical sciences and technology, and in more recent times it has assumed a similar role in the quantitative aspects of the life sciences.
+        Since the 17th century, mathematics has been an indispensable
+        adjunct to the physical sciences and technology, and in more recent
+        times it has assumed a similar role in the quantitative aspects of the life sciences.
       </p>
     </div>
-  </div>
+  </>
 );
 
 export default Home;
